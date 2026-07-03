1968c4a59462d6934722a744f43c93c97a17195f
Nishita <n49480046@gmail.com>
2026-07-03 14:38:15 +0530

    Refactor filter and task input into reusable components

---
+++ src/components/Filter.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+function Filter({ filter, setFilter }) {
+    return (
+        <select
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+        >
+            <option>All</option>
+            <option>Pending</option>
+            <option>Completed</option>
+        </select>
+    );
+}
+
+Filter.propTypes = {
+    filter: PropTypes.string.isRequired,
+    setFilter: PropTypes.func.isRequired,
+};
+
+export default Filter;
